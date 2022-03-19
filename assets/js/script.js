b8021cd60e7d9ef6ee08dea8a8f7243975db617d
pixiepucker <autumnoffic@gmail.com>
2022-03-19 17:32:40 -0500

    rewrote JS file and FINALLY got two api calls to work. network tab successfully displaying data

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,43 +1,44 @@
-//create vars to hold city info and apiKey
+//create vars to hold apiKey, search city btn, and input field
 var apiKey = "3143d8257b41b59979eb92c68e459d58";
-var city = $("#city").text();
+var searchButton = document.querySelector(".saveBtn");
+var cityInput = document.querySelector("#city");
 
+var getForecast = function () {
+    var cityName = $("#city").val();
+    var apiURL = "http://api.openweathermap.org/geo/1.0/direct?q=" +cityName+
+    "&appid=" +apiKey;
 
-//on search button click, run searchCity function
-$('#saveCity').on('click', function(event) {
-    //create var that holds value from input
-    var cityInput = $("#city").val();
-    var citySearchBtn = $("#citySearch").text();
-    //set items to lS to be accessed later and displayed
-    localStorage.setItem(cityInput, JSON.stringify(citySearchBtn));
-
-    //pass event as parameter
-    searchCity(event);
-});
-
-var checkWeather = function () {
-
-};
-
-var searchCity = function (event) {
-    //prevent default action
-    event.preventDefault();
-    //create var to hold api URL
-    var apiURl = "https://api.openweathermap.org/data/2.5/onecall?lat=" + "&lon=" + ".04&exclude=hourly,daily&appid=" + apiKey;
-
-    //use fetch to grab requested data response
-    fetch(apiURl).then(function(response) {
-        //request successful
+    //use fetch to grab json data from apiURL and return it in function(response)
+    fetch(apiURL).then(function(response) {
         if (response.ok) {
             response.json().then(function(data) {
-                console.log("hehe")
+                displayWeather(data);
             });
         } else {
-            //alert user they need to enter valid city
-            alert('Please enter a valid city!')
+            alert("Please enter a valid city!");
         }
-        
-        return data;
+    })
+    .catch(function(error) {
+        alert("Unable to connect to Open Weather API!")
     });
-
 };
+
+var displayWeather = function (data) {
+    var lat = data[0].lat;
+    var lon = data[0].lon;
+    var apiURL = "https://api.openweathermap.org/data/2.5/onecall?lat=" +lat+
+    "&lon=" +lon+ "&exclude=hourly,daily&appid=" + apiKey;
+
+    fetch(apiURL);
+};
+
+//create function that calls getForecast function
+var searchCity = function () {
+    getForecast(cityName);
+
+    var cityName = $("#city").val();
+    console.log(cityName);
+};
+
+//create event listener for search button to run searchCity function
+$(".saveBtn").on('click', searchCity);
