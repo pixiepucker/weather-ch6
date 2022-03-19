3d35405805895335eeaf92304fc91ea4db50b14c
pixiepucker <autumnoffic@gmail.com>
2022-03-19 01:11:54 -0500

    added 2nd js file to hold functions that will eventually load data to DOM, keep getting 400 erros

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,6 +2,12 @@
 $(document).ready(function() {
     //on search button click, run searchCity function
     $('#saveCity').on('click', function(event) {
+        //create var that holds value from input
+        var cityInput = $("#city").val();
+        var citySearchBtn = $("#citySearch").text();
+        //set items to lS to be accessed later and displayed
+        localStorage.setItem(cityInput, JSON.stringify(citySearchBtn));
+
         //pass event as parameter
         searchCity(event);
     });
@@ -11,7 +17,7 @@ var searchCity = function (event) {
     //prevent default action
     event.preventDefault();
     //create var to hold api URL
-    var apiURl = "https://api.openweathermap.org/data/2.5/onecall?appid=2e20e33b3d5d1aa551ca0dfae625e5b0";
+    var apiURl = "https://api.openweathermap.org/data/2.5/onecall?q=${input}&appid=3143d8257b41b59979eb92c68e459d58";
 
     //use fetch to grab requested data response
     fetch(apiURl).then(function(response) {
@@ -21,7 +27,11 @@ var searchCity = function (event) {
                 console.log("hehe")
             });
         } else {
+            //alert user they need to enter valid city
             alert('Please enter a valid city!')
         }
+        
+        return data;
     });
+
 };
