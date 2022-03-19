063df4839f02cc08b8bf21800c249f0cd347a67e
pixiepucker <autumnoffic@gmail.com>
2022-03-19 15:51:14 -0500

    deleted everything, starting over, hating this

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,23 +1,29 @@
-//create a function ready on load
-$(document).ready(function() {
-    //on search button click, run searchCity function
-    $('#saveCity').on('click', function(event) {
-        //create var that holds value from input
-        var cityInput = $("#city").val();
-        var citySearchBtn = $("#citySearch").text();
-        //set items to lS to be accessed later and displayed
-        localStorage.setItem(cityInput, JSON.stringify(citySearchBtn));
+//create vars to hold city info and apiKey
+var apiKey = "3143d8257b41b59979eb92c68e459d58";
+var city = $("#city").text();
 
-        //pass event as parameter
-        searchCity(event);
-    });
+
+//on search button click, run searchCity function
+$('#saveCity').on('click', function(event) {
+    //create var that holds value from input
+    var cityInput = $("#city").val();
+    var citySearchBtn = $("#citySearch").text();
+    //set items to lS to be accessed later and displayed
+    localStorage.setItem(cityInput, JSON.stringify(citySearchBtn));
+
+    //pass event as parameter
+    searchCity(event);
 });
+
+var checkWeather = function () {
+
+};
 
 var searchCity = function (event) {
     //prevent default action
     event.preventDefault();
     //create var to hold api URL
-    var apiURl = "https://api.openweathermap.org/data/2.5/onecall?q=${input}&appid=3143d8257b41b59979eb92c68e459d58";
+    var apiURl = "https://api.openweathermap.org/data/2.5/onecall?lat=" + "&lon=" + ".04&exclude=hourly,daily&appid=" + apiKey;
 
     //use fetch to grab requested data response
     fetch(apiURl).then(function(response) {
