3e655e75527ad9fb68e0e8d73395e183c3375a85
pixiepucker <autumnoffic@gmail.com>
2022-03-19 21:34:20 -0500

    commit before bed just incase

--- assets/js/script.js
+++ assets/js/script.js
@@ -72,11 +72,11 @@ var createDisplay = function(data) {
 var createForecast = function (data) {
     console.log(data);
     //create vars to hold moment
-    var date1 = moment(data[0].dt*1000).format('MMM DD');
-    var date2 = moment(data[1].dt*1000).format('MMM DD');
-    var date3 = moment(data[2].dt*1000).format('MMM DD');
-    var date4 = moment(data[3].dt*1000).format('MMM DD');
-    var date5 = moment(data[4].dt*1000).format('MMM DD');
+    var date1 = moment(data[1].dt*1000).format('MMM DD');
+    var date2 = moment(data[2].dt*1000).format('MMM DD');
+    var date3 = moment(data[3].dt*1000).format('MMM DD');
+    var date4 = moment(data[4].dt*1000).format('MMM DD');
+    var date5 = moment(data[5].dt*1000).format('MMM DD');
     //create date from vars
     $("#date1").text(date1);
     $("#date2").text(date2);
@@ -84,23 +84,23 @@ var createForecast = function (data) {
     $("#date4").text(date4);
     $("#date5").text(date5);
     //create temp from array
-    $("#temp1").text(data[0].temp.day + '\xB0' + ' F');
-    $("#temp2").text(data[1].temp.day + '\xB0' + ' F');
-    $("#temp3").text(data[2].temp.day + '\xB0' + ' F');
-    $("#temp4").text(data[3].temp.day + '\xB0' + ' F');
-    $("#temp5").text(data[4].temp.day + '\xB0' + ' F');
+    $("#temp1").text(data[1].temp.day + '\xB0' + ' F');
+    $("#temp2").text(data[2].temp.day + '\xB0' + ' F');
+    $("#temp3").text(data[3].temp.day + '\xB0' + ' F');
+    $("#temp4").text(data[4].temp.day + '\xB0' + ' F');
+    $("#temp5").text(data[5].temp.day + '\xB0' + ' F');
     //create wind from array
-    $("#wind1").text(data[0].wind_speed + ' MPH');
-    $("#wind2").text(data[1].wind_speed + ' MPH');
-    $("#wind3").text(data[2].wind_speed + ' MPH');
-    $("#wind4").text(data[3].wind_speed + ' MPH');
-    $("#wind5").text(data[4].wind_speed + ' MPH');
+    $("#wind1").text(data[1].wind_speed + ' MPH');
+    $("#wind2").text(data[2].wind_speed + ' MPH');
+    $("#wind3").text(data[3].wind_speed + ' MPH');
+    $("#wind4").text(data[4].wind_speed + ' MPH');
+    $("#wind5").text(data[5].wind_speed + ' MPH');
     //create humidity from array
-    $("#hum1").text(data[0].humidity + ' %');
-    $("#hum2").text(data[1].humidity + ' %');
-    $("#hum3").text(data[2].humidity + ' %');
-    $("#hum4").text(data[3].humidity + ' %');
-    $("#hum5").text(data[4].humidity + ' %');
+    $("#hum1").text(data[1].humidity + ' %');
+    $("#hum2").text(data[2].humidity + ' %');
+    $("#hum3").text(data[3].humidity + ' %');
+    $("#hum4").text(data[4].humidity + ' %');
+    $("#hum5").text(data[5].humidity + ' %');
 };
 
 //create function that calls getForecast function
